efe5dce36234cfb31cf3fb175f823e06021261e8
OLIVIER MANGILA <oliviermangila1@gmail.com>
2026-03-12 23:32:52 +0100

    add qr code display

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
-import makeWASocket, { DisconnectReason, useMultiFileAuthState } from "@whiskeysockets/baileys"
+import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys"
 import pino from "pino"
+import QRCode from "qrcode"
 
 async function startBot() {
 
 const { state, saveCreds } = await useMultiFileAuthState("session")
 
 const sock = makeWASocket({
-logger: pino({ level: "silent" }),
 auth: state,
-printQRInTerminal: true
+logger: pino({ level: "silent" })
+})
+
+sock.ev.on("connection.update", async (update) => {
+
+const { qr, connection } = update
+
+if (qr) {
+console.log("SCAN QR CODE:")
+console.log(await QRCode.toString(qr,{type:"terminal"}))
+}
+
+if (connection === "open") {
+console.log("BOT CONNECTÉ ✅")
+}
+
 })
 
 sock.ev.on("creds.update", saveCreds)
-
-sock.ev.on("connection.update", (update) => {
-
-const { connection, lastDisconnect } = update
-
-if (connection === "close") {
-
-const shouldReconnect =
-(lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
-
-if (shouldReconnect) {
-startBot()
-}
-
-} else if (connection === "open") {
-
-console.log("OLIMAX 2.0 BOT ACTIF 🚀")
-
-}
-
-})
-
-sock.ev.on("messages.upsert", async ({ messages }) => {
-
-const msg = messages[0]
-
-if (!msg.message) return
-
-const text =
-msg.message.conversation ||
-msg.message.extendedTextMessage?.text
-
-if (text === "ping") {
-
-await sock.sendMessage(msg.key.remoteJid, {
-text: "pong 🟢 OLIMAX actif"
-})
-
-}
-
-})
 
 }
 
