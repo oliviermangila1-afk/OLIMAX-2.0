7c2a2bafb4553229dd70d1f4edd94079125cbfd0
OLIVIER MANGILA <oliviermangila1@gmail.com>
2026-03-15 20:22:56 +0100

    pairing login

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ useMultiFileAuthState
 } from "@whiskeysockets/baileys"
 
 import pino from "pino"
-import qrcode from "qrcode-terminal"
 
 const BOT_NAME = "OLIMAX-2.0"
 const OWNER_NAME = "OLIVIER MANGILA"
@@ -26,49 +25,22 @@ browser: [BOT_NAME,"Chrome","1.0"]
 
 sock.ev.on("creds.update", saveCreds)
 
-sock.ev.on("connection.update", (update)=>{
+if(!sock.authState.creds.registered){
 
-const { connection, qr } = update
+const phoneNumber = "243981240435"
 
-if(qr){
-console.log("Scanne ce QR avec WhatsApp")
-qrcode.generate(qr,{small:true})
+const code = await sock.requestPairingCode(phoneNumber)
+
+console.log("CODE DE CONNEXION :", code)
+
 }
+
+sock.ev.on("connection.update",(update)=>{
+
+const { connection } = update
 
 if(connection === "open"){
 console.log("✅ Bot connecté à WhatsApp")
-}
-
-})
-
-/* ===== REPONSES AUTOMATIQUES ===== */
-
-sock.ev.on("messages.upsert", async ({ messages }) => {
-
-const msg = messages[0]
-
-if(!msg.message) return
-
-const text =
-msg.message.conversation ||
-msg.message.extendedTextMessage?.text
-
-if(!text) return
-
-const message = text.toLowerCase()
-
-if(message.includes("qui t'a créé") || message.includes("creator") || message.includes("owner")){
-
-await sock.sendMessage(msg.key.remoteJid,{
-text: `🤖 Je suis *${BOT_NAME}*
-
-👨‍💻 Créé par : *${OWNER_NAME}*
-
-📞 Si vous voulez la même IA ou un bot WhatsApp, contactez-le :
-
-${OWNER_NUMBER}`
-})
-
 }
 
 })
